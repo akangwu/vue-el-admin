261d029218f0f8565f76d12beec1f70259c0e432
akangwu <akangwu@163.com>
2020-03-25 22:27:41 +0800

    14后台主布局开发

--- src/views/layout.vue
+++ src/views/layout.vue
@@ -1,8 +1,20 @@
 <template>
   <div>
-    首页
+    <el-container style="position: absolute; top:0;right:0;bottom:0;left:0;overflow:hidden">
+      <el-header>Header</el-header>
+      <el-container style="height: 100%;padding-bottom: 60px">
+<!--        侧边布局-->
+        <el-aside width="200px">
+          <li v-for="i in 100" :key="i">{{i}}</li>
+        </el-aside>
+<!--        主布局-->
+        <el-main>
+          <li v-for="i in 100" :key="i">{{i}}</li>
+        </el-main>
+      </el-container>
+    </el-container>
 
-    <router-view></router-view>
+<!--    <router-view></router-view>-->
   </div>
 </template>
 
@@ -13,5 +25,19 @@
 </script>
 
 <style scoped>
+  .el-header {
+    background-color: #B3C0D1;
+    color: #333;
+  }
 
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+  }
 </style>
