d9eff2da9d8ba4df7b811d3e8ce3182d5094e927
akangwu <akangwu@163.com>
2020-03-26 22:07:44 +0800

    16全局侧边导航栏开发(一)

--- src/views/layout.vue
+++ src/views/layout.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
     <el-container style="position: absolute; top:0;right:0;bottom:0;left:0;overflow:hidden">
-<!--      头部导航栏，这里和复制的有眼色差别，给一个背景色相同的就可以了-->
+      <!--      头部导航栏，这里和复制的有眼色差别，给一个背景色相同的就可以了-->
       <el-header class="d-flex align-items-center" style="background:#545c64">
-<!--        顶部导航栏，直接复制的-->
+        <!--        顶部导航栏，直接复制的-->
         <a class="h5 text-light mb-0 mr-auto">UNI-ADMIN</a>
+        <!--激活时的线-->
         <div class="line"></div>
         <el-menu
           :default-active="navBarIndex"
@@ -22,7 +23,8 @@
           <el-submenu index="100">
             <template slot="title">
               <el-avatar size="small" style="padding-right: 10px;margin-right: 10px;"
-                         src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"></el-avatar>豆沙小饼干
+                         src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"></el-avatar>
+              豆沙小饼干
             </template>
             <el-menu-item index="100-1">修改</el-menu-item>
             <el-menu-item index="100-2">退出</el-menu-item>
@@ -30,32 +32,51 @@
         </el-menu>
       </el-header>
       <el-container style="height: 100%;padding-bottom: 60px">
-<!--        侧边布局-->
+        <!--侧边布局-->
         <el-aside width="200px">
-          <li v-for="i in 100" :key="i">{{i}}</li>
+              <el-menu
+                default-active="2"
+                @select="slideSelect">
+                <el-menu-item index="1">
+                  <i class="el-icon-menu"></i>
+                  <span slot="title">导航</span>
+                </el-menu-item>
+                <el-menu-item index="2">
+                  <i class="el-icon-document"></i>
+                  <span slot="title">导航三</span>
+                </el-menu-item>
+                <el-menu-item index="3">
+                  <i class="el-icon-setting"></i>
+                  <span slot="title">导航四</span>
+                </el-menu-item>
+
+              </el-menu>
         </el-aside>
-<!--        主布局-->
+        <!--        主布局-->
         <el-main>
           <li v-for="i in 100" :key="i">{{i}}</li>
         </el-main>
       </el-container>
     </el-container>
 
-<!--    <router-view></router-view>-->
+    <!--    <router-view></router-view>-->
   </div>
 </template>
 
 <script>
   export default {
     name: 'layout',
-    data() {
+    data () {
       return {
         navBarIndex: '1',
       }
     },
     methods: {
-      handleSelect(key, keyPath) {
-        console.log(key, keyPath);
+      handleSelect (key, keyPath) {
+        console.log(key, keyPath)
+      },
+      slideSelect (key, keyPath) {
+        console.log(key, keyPath)
       }
     }
 
