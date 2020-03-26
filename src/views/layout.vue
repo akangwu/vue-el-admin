a6742fe4bccd18e290dc060059f12e0777facb89
akangwu <akangwu@163.com>
2020-03-26 22:35:46 +0800

    17全局顶部导航栏开发(二)

--- src/views/layout.vue
+++ src/views/layout.vue
@@ -1,24 +1,22 @@
 <template>
   <div>
     <el-container style="position: absolute; top:0;right:0;bottom:0;left:0;overflow:hidden">
-      <!--      头部导航栏，这里和复制的有眼色差别，给一个背景色相同的就可以了-->
+      <!--头部导航栏，这里和复制的有眼色差别，给一个背景色相同的就可以了-->
       <el-header class="d-flex align-items-center" style="background:#545c64">
-        <!--        顶部导航栏，直接复制的-->
+        <!--顶部导航栏，直接复制的-->
         <a class="h5 text-light mb-0 mr-auto">UNI-ADMIN</a>
         <!--激活时的线-->
         <div class="line"></div>
         <el-menu
-          :default-active="navBarIndex"
+          :default-active="navBar.active"
           mode="horizontal"
           @select="handleSelect"
           background-color="#545c64"
           text-color="#fff"
           active-text-color="#ffd04b">
-          <el-menu-item index="1">首页</el-menu-item>
-          <el-menu-item index="2">商品</el-menu-item>
-          <el-menu-item index="3">订单</el-menu-item>
-          <el-menu-item index="4">会员</el-menu-item>
-          <el-menu-item index="5">设置</el-menu-item>
+          <el-menu-item :index="index|numToString" v-for="(item,index) in navBar.list" :key="index">
+            {{item.name}}
+          </el-menu-item>
 
           <el-submenu index="100">
             <template slot="title">
@@ -34,23 +32,23 @@
       <el-container style="height: 100%;padding-bottom: 60px">
         <!--侧边布局-->
         <el-aside width="200px">
-              <el-menu
-                default-active="2"
-                @select="slideSelect">
-                <el-menu-item index="1">
-                  <i class="el-icon-menu"></i>
-                  <span slot="title">导航</span>
-                </el-menu-item>
-                <el-menu-item index="2">
-                  <i class="el-icon-document"></i>
-                  <span slot="title">导航三</span>
-                </el-menu-item>
-                <el-menu-item index="3">
-                  <i class="el-icon-setting"></i>
-                  <span slot="title">导航四</span>
-                </el-menu-item>
+          <el-menu
+            default-active="2"
+            @select="slideSelect">
+            <el-menu-item index="1">
+              <i class="el-icon-menu"></i>
+              <span slot="title">导航</span>
+            </el-menu-item>
+            <el-menu-item index="2">
+              <i class="el-icon-document"></i>
+              <span slot="title">导航三</span>
+            </el-menu-item>
+            <el-menu-item index="3">
+              <i class="el-icon-setting"></i>
+              <span slot="title">导航四</span>
+            </el-menu-item>
 
-              </el-menu>
+          </el-menu>
         </el-aside>
         <!--        主布局-->
         <el-main>
@@ -64,11 +62,22 @@
 </template>
 
 <script>
+  import common from '../common/mixins/common.js'
   export default {
+    mixins: [common],
     name: 'layout',
     data () {
       return {
-        navBarIndex: '1',
+        navBar: {
+          active: '0',
+          list: [
+            {name: '首页'},
+            {name: '商品'},
+            {name: '订单'},
+            {name: '会员'},
+            {name: '设置'}
+          ]
+        }
       }
     },
     methods: {
