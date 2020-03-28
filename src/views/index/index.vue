cba48d9dc35b086325df43968fbe26729c3afbbf
akangwu <akangwu@163.com>
2020-03-28 21:18:25 +0800

    23后台主板界面UI构建(一)

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,13 +1,40 @@
 <template>
   <div>
-      <h3>      我是后台首页
-      </h3>
+  <!--数据统计-->
+    <el-row :gutter="10">
+      <el-col :span="6" v-for="(item,index) in counts" :key="index">
+        <el-card shadow="hover">
+          <div class="d-flex align-items-center">
+            <div>
+              <i class="h4 mb-0 text-white mr-3"
+                 :class="[item.icon,item.color]"
+                 style="width: 40px;height: 40px; text-align: center; line-height: 40px;"></i>
+            </div>
+            <div>
+              <h4 class="mb-1">{{item.num}}</h4>
+              <small class="text-muted">{{item.desc}}</small>
+            </div>
+          </div>
+        </el-card>
+
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'index'
+    name: 'index',
+    data() {
+      return {
+        counts: [
+          {icon: 'el-icon-user-solid ',num: '30',desc: '关注人数(个)',color: 'bg-primary'},
+          {icon: 'el-icon-s-finance ',num: '120',desc: '订单总数(笔)',color: 'bg-success'},
+          {icon: 'el-icon-s-order ',num: '4183.80',desc: '今日订单总金额(元)',color: 'bg-danger'},
+          {icon: 'el-icon-s-data ',num: '100',desc: '本月销量(笔)',color: 'bg-warning'}
+        ]
+      }
+    }
   }
 </script>
 
