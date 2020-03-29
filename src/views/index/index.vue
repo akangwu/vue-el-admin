2424cbe302931cb00106377460f9c18a3aac78cc
akangwu <akangwu@163.com>
2020-03-29 10:33:32 +0800

    26安装echarts和使用

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -32,7 +32,7 @@
           </div>
           <div class="row">
             <div :class="tip.list.length | getCol" v-for="(tlist,listi) in tip.list" :key="listi">
-            <!--<div class="col-3" v-for="(tlist,listi) in tip.list" :key="listi">-->
+              <!--<div class="col-3" v-for="(tlist,listi) in tip.list" :key="listi">-->
               <button class="btn btn-light w-100">
                 <h4>{{tlist.value}}</h4>
                 <small class="text-muted">{{tlist.name}}</small>
@@ -44,7 +44,12 @@
       <!--统计图-->
       <el-col :span="12">
         <el-card class="box-card" style="height:370px" shadow="never">
-          <div>3</div>
+          <div slot="header">
+            <span>卡片名称</span>
+            <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+          </div>
+          <!-- 为 ECharts 准备一个具备大小（宽高）的 DOM -->
+          <div ref="myChart" style="width: 100%;height:270px;"></div>
         </el-card>
       </el-col>
     </el-row>
@@ -52,6 +57,8 @@
 </template>
 
 <script>
+  import echarts from 'echarts'
+
   export default {
     name: 'index',
     data () {
@@ -67,35 +74,112 @@
             title: '店铺及商品提示',
             desc: '需要关注的店铺信息及待处理事项',
             list: [
-              {name: '出售中', value:'64'},
-              {name: '待回复', value:'10'},
-              {name: '库存预警', value:'0'},
-              {name: '仓库中', value:'3'}
+              {name: '出售中', value: '64'},
+              {name: '待回复', value: '10'},
+              {name: '库存预警', value: '0'},
+              {name: '仓库中', value: '3'}
             ]
           },
           {
             title: '交易提示',
             desc: '您需要立即处理的交易订单',
             list: [
-              {name: '待付款', value:'0'},
-              {name: '待发货', value:'10'},
-              {name: '已发货', value:'0'},
-              {name: '已收货', value:'3'},
-              {name: '退款中', value:'3'},
-              {name: '待售后', value:'3'}
+              {name: '待付款', value: '0'},
+              {name: '待发货', value: '10'},
+              {name: '已发货', value: '0'},
+              {name: '已收货', value: '3'},
+              {name: '退款中', value: '3'},
+              {name: '待售后', value: '3'}
             ]
           }
-        ]
+        ],
+
       }
     },
+    mounted () {
+      //  画统计图
+      this.drawLine()
+    },
     filters: {
-      getCol(total) {
-        return `col-${12/total}`
+      getCol (total) {
+        return `col-${12 / total}`
       }
     },
     methods: {
-      getCols () {
-        console.log()
+      drawLine () {
+        console.log(this.$refs.myChart)
+        let myChart = echarts.init(this.$refs.myChart)
+        myChart.setOption({
+          legend: {
+            data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
+          },
+          toolbox: {
+            feature: {
+              saveAsImage: {}
+            }
+          },
+          grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+          },
+          xAxis: [
+            {
+              type: 'category',
+              boundaryGap: false,
+              data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+            }
+          ],
+          yAxis: [
+            {
+              type: 'value'
+            }
+          ],
+          series: [
+            {
+              name: '邮件营销',
+              type: 'line',
+              stack: '总量',
+              areaStyle: {},
+              data: [120, 132, 101, 134, 90, 230, 210]
+            },
+            {
+              name: '联盟广告',
+              type: 'line',
+              stack: '总量',
+              areaStyle: {},
+              data: [220, 182, 191, 234, 290, 330, 310]
+            },
+            {
+              name: '视频广告',
+              type: 'line',
+              stack: '总量',
+              areaStyle: {},
+              data: [150, 232, 201, 154, 190, 330, 410]
+            },
+            {
+              name: '直接访问',
+              type: 'line',
+              stack: '总量',
+              areaStyle: {},
+              data: [320, 332, 301, 334, 390, 330, 320]
+            },
+            {
+              name: '搜索引擎',
+              type: 'line',
+              stack: '总量',
+              label: {
+                normal: {
+                  show: true,
+                  position: 'top'
+                }
+              },
+              areaStyle: {},
+              data: [820, 932, 901, 934, 1290, 1330, 1320]
+            }
+          ]
+        })
       }
     }
   }
