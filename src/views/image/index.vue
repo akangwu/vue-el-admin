8c4a55a0636ff30f3821ab0df06368e08b654998
akangwu <akangwu@163.com>
2020-03-29 14:06:56 +0800

    29相册管理界面UI构建(二)

--- src/views/image/index.vue
+++ src/views/image/index.vue
@@ -1,12 +1,28 @@
 <template>
   <div>
     <el-container style="position: absolute; top: 55px;right: 0;bottom: 0; left: 0;">
-      <el-header>Header</el-header>
+      <!--头部,先看看有没有flex布局-->
+      <el-header class="d-flex align-items-center">
+        <div class="d-flex mr-auto">
+          <el-select v-model="value" clearable placeholder="请选择" style="width: 150px" class="mr-2">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="北京"></el-option>
+          </el-select>
+          <el-input style="width:250px" class="mr-2"></el-input>
+          <el-button type="primary" size="small">搜索</el-button>
+        </div>
+        <div>
+          <el-button type="success" size="medium">创建相册</el-button>
+          <el-button type="warning" size="medium">上传图片</el-button>
+        </div>
+      </el-header>
       <el-container>
+        <!--侧边-->
         <el-aside width="200px" style="position: absolute;top: 60px;left: 0;bottom: 60px;">
           <div style="height:1000px"></div>
         </el-aside>
         <el-container>
+          <!--内容-->
           <el-main  style="position: absolute;top: 60px;right: 0;left: 200px;bottom: 60px;">
             <div style="height:1000px"></div>
 
@@ -26,18 +42,5 @@
 </script>
 
 <style scoped>
-  .el-header, .el-footer {
-    background-color: #B3C0D1;
-    color: #333;
-  }
 
-  .el-aside {
-    background-color: #D3DCE6;
-    color: #333;
-  }
-
-  .el-main {
-    background-color: #E9EEF3;
-    color: #333;
-  }
 </style>
